260136255c98154e2d1ee6b2b23660126c8e3e83
Ian Kolaja <iankolaja@gmail.com>
2023-09-20 22:16:28 -0700

    Added function for extracting data from bumat

--- pearlsim/ml_utilities.py
+++ pearlsim/ml_utilities.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from pearlsim.material import get_cross_section_string
 
 def read_det_file(file_name, energy_bins_lower=(0.0, 3e-08, 5.8e-08, 1.4e-07, 2.8e-07, 3.5e-07, 6.25e-07,
                                             4e-06, 4.8052e-05, 0.00553, 0.821, 2.231)):
@@ -44,3 +44,51 @@ def read_det_file(file_name, energy_bins_lower=(0.0, 3e-08, 5.8e-08, 1.4e-07, 2.
     targets = pd.DataFrame(pebble_flux_matrix, columns=energy_bins_lower)
     avg_uncertainty = np.mean(np.array(unc_array))
     return features, targets, avg_uncertainty
+
+
+def extract_from_bumat(file_path):
+    concentrations = {}
+    with open(file_path, 'r') as f:
+        reading = False
+        first_mat = True
+        for line in f:
+            line = line.split()
+            if len(line) == 0:
+                continue
+            if line[0] == "mat":
+                if not first_mat:
+                    concentrations[current_mat_name] = current_conc
+                current_mat_name = line[1].split("pp")[0]
+                current_conc = {}
+                reading = True
+                first_mat = False
+            elif reading:
+                id = line[0].split(".")
+                if len(id) > 1:
+                    nuclide = id[0] + "<lib>"
+                else:
+                    nuclide = id[0]
+                amount = float(line[1].replace("\n", ""))
+                current_conc[nuclide] = amount
+    concentrations[current_mat_name] = current_conc
+    return concentrations
+
+def generate_pebble_burnup_model(template_path, surface_current, concentrations, temp, time,
+                                 energy_bins=[3e-08, 5.8e-08, 1.4e-07, 2.8e-07, 3.5e-07, 6.25e-07,
+                                              4e-06, 4.8052e-05, 0.00553, 0.821, 2.231, 1e+37]):
+    lib_str = get_cross_section_string(temp)
+    with open(template_path, "r") as f:
+        input_s = f.read()
+    concentration_s = ""
+    for key in concentrations.keys():
+        concentration_s  += f"  {key}    {concentrations[key]}\n".replace("<lib>", lib_str)
+    input_s = input_s.replace("<concentrations>", concentration_s)
+    input_s = input_s.replace("<temperature>", temp)
+    input_s = input_s.replace("<time>", time)
+    input_s = input_s.replace("<rate>", str(np.sum(surface_current)))
+    weights = surface_current/np.sum(surface_current)
+    source_str = "  0 0\n"
+    for i in range(len(weights)):
+        source_str += f"  {energy_bins[i]} {weights[i]}\n"
+    input_s = input_s.replace("<current>", source_str)
+    return input_s

6f1c7b80f2c54ba7da3f59a35f5b4c32db8b9c0d
Ian Kolaja <iankolaja@gmail.com>
2023-10-04 21:36:10 -0700

    switched to argparse

--- PEARLSim.py
+++ PEARLSim.py
@@ -1,48 +1,30 @@
 from pearlsim.simulation import Simulation
-import getopt, sys
+import argparse
 
-# Remove 1st argument from the
-# list of command line arguments
-argumentList = sys.argv[1:]
-
-# Options
-options = "fcnd:"
-
-# Long options
-long_options = ["file", "cores", "nodes", "debug"]
-
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file')
+parser.add_argument('-c', '--cores')
+parser.add_argument('-n', '--nodes')
+parser.add_argument('-d', '--debug')
+parser.add_argument('-v', dest='verbose', action='store_true')
+args = parser.parse_args()
 simulation_name = "gFHR_equilibrium"
 simulation = Simulation(simulation_name)
 
-num_cores = 20
 
-try:
-    # Parsing argument
-    arguments, values = getopt.getopt(argumentList, options, long_options)
-
-    # checking each argument
-    for currentArgument, currentValue in arguments:
-
-        if currentArgument in ("-f", "--file"):
-            print("Reading input file:", sys.argv[1])
-            input_file = sys.argv[1]
-
-        elif currentArgument in ("-c", "--cores"):
-            print("Number of cores:", sys.argv[1])
-            cores = int(sys.argv[1])
-            simulation.cpu_cores = cores
-
-        elif currentArgument in ("-n", "--nodes"):
-            print("Number of nodes:", sys.argv[1])
-            nodes = int(sys.argv[1])
-            simulation.num_nodes = nodes
-
-        elif currentArgument in ("-d", "--debug"):
-            print("Setting debug level:", sys.argv[1])
-            simulation.debug = int(sys.argv[1])
-
-except getopt.error as err:
-    # output error, and return with an error code
-    print(str(err))
+args = parser.parse_args()
+ 
+if args.file:
+    print("Reading input file:", args.file)
+    input_file = args.file
+if args.cores:
+    print("Setting number of cores", args.cores)
+    simulation.num_cores = args.cores
+if args.nodes:
+    print("Setting number of nodes", args.nodes)
+    simulation.num_nodes = args.nodes
+if args.debug:
+    print("Setting debug level", args.debug)
+    simulation.debug = args.debug
 
 simulation.read_input_file(input_file)

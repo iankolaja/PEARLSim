c686d4404e0737eec73b493c2cd7a51eda45b8c6
Ian Kolaja <iankolaja@gmail.com>
2023-10-04 21:55:12 -0700

    fixed overlapping arguements

--- PEARLSim.py
+++ PEARLSim.py
@@ -3,11 +3,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file')
-parser.add_argument('-d', '--dir')
+parser.add_argument('-w', '--workingdir')
 parser.add_argument('-c', '--cores')
 parser.add_argument('-n', '--nodes')
 parser.add_argument('-d', '--debug')
-parser.add_argument('-v', dest='verbose', action='store_true')
 args = parser.parse_args()
 simulation_name = "gFHR_equilibrium"
 simulation = Simulation(simulation_name)
@@ -18,9 +17,9 @@ args = parser.parse_args()
 if args.file:
     print("Reading input file:", args.file)
     input_file = args.file
-if args.dir:
-    print("Working in directory", args.dir)
-    directory = args.dir
+if args.workingdir:
+    print("Working in directory", args.workingdir)
+    directory = args.workingdir
 if args.cores:
     print("Setting number of cores", args.cores)
     simulation.num_cores = args.cores
